f2094522b3940fd361ed9799bd0fcf4ba2b84988
Edgar <Edgar@AnotherFoxGuy.com>
2020-05-15 13:20:36 +0200

    :bug: Fixed build

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
+    builder.build_policy = "missing"
     builder.add_common_builds()
     builder.run()
