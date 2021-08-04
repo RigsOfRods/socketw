8927090371d21b748d953f8c70574d9557b7c129
Edgar <Edgar@AnotherFoxGuy.com>
2021-08-04 20:01:50 +0200

    :fire: Removed conan package

--- build.py
+++
@@ -1,8 +0,0 @@
-from conan.packager import ConanMultiPackager
-
-
-if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.build_policy = "missing"
-    builder.add_common_builds()
-    builder.run()
